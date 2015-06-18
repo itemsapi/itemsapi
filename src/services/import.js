522199c871f279cf029164ca2c0aacbcf132ff31
Mateusz Rzepa <mrzepa89@gmail.com>
2015-06-18 12:41:35 +0200

    intendation

--- src/services/import.js
+++ src/services/import.js
@@ -8,36 +8,30 @@ var async = require('async');
 
 (function(module) {
 
-    /**
-     * import json and create full project
-     */
-    module.import = function(data, callback) {
+  /**
+   * import json and create full project
+   * ensure project
+   * ensure mapping
+   * add documents
+   * show statistics
+   */
+  module.import = function(data, callback) {
 
-        var mapping = data.mapping;
-        var documents = data.documents;
+    projectService.ensureCollection(data.mapping, function(err, res) {
+      if (err) {
+        return callback(err);
+      }
 
-        projectService.addTogether(mapping, function(err, res) {
-            if (err) {
-                return callback(err);
-            }
-
-
-            var docs = {
-                project_name: mapping.project_name,
-                table_name: mapping.table_name,
-                documents: documents
-            };
-
-            dataService.addAllDocuments(docs, function(err, res) {
-                if (err) {
-                    return callback(err);
-                }
-                callback(null, res);
-            });
-
-        });
-
-    }
-
+      dataService.addAllDocuments({
+        projectName: mapping.project_name,
+        collectionName: mapping.table_name,
+        documents: documents
+      }, function(err, res) {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, res);
+      });
+    });
+  }
 }(exports));
-
