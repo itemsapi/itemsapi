5555a1ad9311c57f044d511a9440868927316c4b
Mateusz Rzepa <mrzepa89@gmail.com>
2016-03-15 21:51:49 +0100

    elasticsearch async refactoring

--- src/connections/elastic.js
+++ src/connections/elastic.js
@@ -5,11 +5,24 @@
   var elasticsearch = require('elasticsearch');
   var logger = require('../../config/logger');
   var client;
+  var Promise = require('bluebird');
 
   module.init = function(conf) {
     try {
       //https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/current/configuration.html
       conf.apiVersion = '1.7';
+      conf.defer = function () {
+        var resolve, reject;
+        var promise = new Promise(function() {
+          resolve = arguments[0];
+          reject = arguments[1];
+        });
+        return {
+          resolve: resolve,
+          reject: reject,
+          promise: promise
+        };
+      }
       client = new elasticsearch.Client(conf);
     } catch (err) {
       logger.error('Unable to initialize elasticsearch! Error :' + err.message);
--- src/elastic/data.js
+++ src/elastic/data.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Promise = require('bluebird');
-var elastic = Promise.promisifyAll(require('../connections/elastic').getElastic());
+var elastic = require('../connections/elastic').getElastic();
 var validate = require('validate.js');
 
 (function(module) {
@@ -125,7 +125,7 @@ var validate = require('validate.js');
    * @param {Obj} data document
    */
   module.countDocumentsAsync = function(data, callback) {
-    return elastic.countAsync({
+    return elastic.count({
       index: data.index,
       type: data.type,
     }).then(function(res) {
@@ -137,8 +137,8 @@ var validate = require('validate.js');
    * clean documents
    * @param {Obj} data document
    */
-  module.cleanDocumentsAsync = function(data, callback) {
-    return elastic.deleteByQueryAsync({
+  module.cleanDocuments = function(data, callback) {
+    return elastic.deleteByQuery({
       index: data.index,
       type: data.type,
       body: {
